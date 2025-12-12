bee6c2b5d802b248df4954b46403ea17e1193baf
Skzter <v.goetze03@gmail.com>
2025-12-12 11:40:37 +0100

    feat: reset app

--- app/src/main/java/com/example/starcatalog/screens/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.starcatalog.screens
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Options : Screen("options")
-    object Find : Screen("find")
-    object Visited : Screen("visited")
-}
