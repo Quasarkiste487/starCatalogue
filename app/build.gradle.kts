47e552acbcb86cbb5cfd04ee78281ff65b276952
Skzter <v.goetze03@gmail.com>
2025-12-12 12:50:01 +0100

    feat: adds first steps for api access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -58,4 +60,15 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // Navigation
+    implementation(libs.navigation)
+    implementation(libs.kotlinx.serialization.core)
+
+    // Networking
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.moshiConverter)
+    implementation(libs.retrofit.loggingInterceptor)
+    implementation(libs.moshi.core)
+    implementation(libs.moshi.adapters)
+    ksp(libs.moshi.annotationProcessor)
 }
---
+++ app/src/main/kotlin/com/example/starccatalogue/ui/stars/StarsRoute.kt
@@ -0,0 +1,6 @@
+package com.example.starccatalogue.ui.stars
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object StarsRoute
