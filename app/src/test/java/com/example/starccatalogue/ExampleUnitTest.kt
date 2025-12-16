55c7badc5b0d209403e101d3f322234bf4b3f090
Bruno Bittner <bruno.bittner@stud.htwk-leipzig.de>
2025-12-17 00:07:51 +0100

    stardata from simbad

--- app/src/test/java/com/example/starccatalogue/ExampleUnitTest.kt
+++ app/src/test/java/com/example/starccatalogue/ExampleUnitTest.kt
@@ -1,8 +1,16 @@
 package com.example.starccatalogue
 
 import org.junit.Test
+import uk.ac.starlink.table.StarTable
+import uk.ac.starlink.table.StarTableFactory
+import uk.ac.starlink.votable.VOTableBuilder
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import kotlin.time.measureTime
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +18,96 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    var base = "https://simbad.cds.unistra.fr/simbad/sim-script"
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun fetchStardata() {
+        val script = Script(10, listOf("main_id", "coordinates", "flux(V)"), "Vmag < 6").build()
+
+        val url = base + "?script=" + URLEncoder.encode(script, StandardCharsets.UTF_8)
+        val connection = URL(url).openConnection()
+        var stream : InputStream
+        var meta : Map<String, String>
+        var table : StarTable
+
+        println("request time: " + measureTime {
+            stream = connection.getInputStream()
+        })
+
+        println("header parsing time " + measureTime {
+            meta = parseStream(stream)
+        })
+
+        //println("body")
+        //stream.bufferedReader().forEachLine { println(it) }
+
+        println("table building time: " + measureTime {
+            table = StarTableFactory().makeStarTable(stream, VOTableBuilder())
+        })
+
+        println(meta)
+        writeTable(table)
     }
+}
+
+fun parseStream(inputStream: InputStream) : Map<String, String>{
+    val metadata = HashMap<String, String>()
+    var currentSegment : String? = null
+
+    val buffer = StringBuilder()
+
+    for (line in inputStream.readLinesStrictUntil("<\\?xml.*\\?>".toRegex())){
+        if (line.startsWith("::")){ // simplify check
+            // Process previous buffer before starting new segment
+            if (currentSegment != null){
+                // println("Finished $currentSegment with ${buffer.length} chars")
+                metadata[currentSegment] = buffer.toString()
+            }
+            currentSegment = line.substringAfter("::").substringBefore(":")
+            buffer.clear()
+        } else {
+            buffer.appendLine(line)
+        }
+    }
+    return metadata
+}
+
+fun InputStream.readLinesStrictUntil(delim: Regex): List<String> {
+    val lines = mutableListOf<String>()
+    do {
+        val sb = StringBuilder()
+        var byte: Int
+        while (true) {
+            byte = this.read()
+            if (byte == -1 || byte == '\n'.code) break
+            if (byte != '\r'.code) sb.append(byte.toChar())
+        }
+        if (byte == -1 && sb.isEmpty()) break
+        lines.add(sb.toString())
+    } while (!delim.matches(sb.toString()))
+    return lines
+}
+
+class Script(val limit : Int, val fields: List<String>, val criteria: String){
+    public fun build() : String =
+        """
+        set limit $limit
+        votable ${fields.joinToString(separator = ",", prefix = "{", postfix = "}")}
+        votable open
+        query sample $criteria
+    """.trimIndent()
+}
+
+@Throws(IOException::class)
+fun writeTable(table: StarTable) {
+    val nCol = table.columnCount
+    val rseq = table.rowSequence
+    while (rseq.next()) {
+        val row = rseq.row
+        for (icol in 0..<nCol) {
+            print(row[icol].toString() + "\t")
+        }
+        println()
+    }
+    rseq.close()
 }
