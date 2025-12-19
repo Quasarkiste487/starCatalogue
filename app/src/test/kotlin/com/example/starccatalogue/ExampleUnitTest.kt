f6478367b2616fc4be1d071fab71bcda08c64f6b
Skzter <v.goetze03@gmail.com>
2025-12-19 10:24:23 +0100

    refactor: move files from java to kotlin dir

--- app/src/test/kotlin/com/example/starccatalogue/ExampleUnitTest.kt
+++ app/src/test/kotlin/com/example/starccatalogue/ExampleUnitTest.kt
@@ -4,15 +4,6 @@ import com.example.starccatalogue.network.Script
 import com.example.starccatalogue.network.Simbad
 import com.example.starccatalogue.network.printAll
 import org.junit.Test
-import uk.ac.starlink.table.StarTable
-import uk.ac.starlink.table.StarTableFactory
-import uk.ac.starlink.votable.VOTableBuilder
-import java.io.IOException
-import java.io.InputStream
-import java.net.URL
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
-import kotlin.time.measureTime
 
 
 /**
